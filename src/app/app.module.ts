8428de02dcc9f0406efe082e0950a4165328f4f2
azrin-dev <me@azrin.dev>
2019-10-25 17:04:20 +0800

    Added material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Module 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+// Component
 import { HomeComponent } from './home/home.component';
 import { MenuSidenavComponent } from './menu/menu-sidenav/menu-sidenav.component';
 
@@ -18,7 +20,8 @@ import { MenuSidenavComponent } from './menu/menu-sidenav/menu-sidenav.component
    imports: [
       BrowserModule,
       AppRoutingModule,
-      BrowserAnimationsModule
+      BrowserAnimationsModule,
+      MaterialModule 
    ],
    providers: [],
    bootstrap: [AppComponent]
