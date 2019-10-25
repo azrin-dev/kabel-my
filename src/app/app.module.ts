f16a6c8046c8acd0f5190c2b63cbc9bdbde18fa6
azrin-dev <me@azrin.dev>
2019-10-25 16:27:20 +0800

    Added menu router outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
+import { MenuSidenavComponent } from './menu/menu-sidenav/menu-sidenav.component';
 
 
 @NgModule({
 
    declarations: [
       AppComponent,
-      HomeComponent
+      HomeComponent,
+      MenuSidenavComponent
    ],
    imports: [
       BrowserModule,
